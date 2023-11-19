67c50081e22e6866968dda899d598b701447c130
Code Sandbox <hello@codesandbox.io>
2023-11-19 06:04:01 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.f0bdfbd92027d3f12f58728eadf52098.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bf643bd5d9aa868e771276f0e052d7f",
+    "url": "https://.github.io/csb-l46wcf/index.html"
+  },
+  {
+    "revision": "88171315aff8383c99ce",
+    "url": "https://.github.io/csb-l46wcf/static/js/2.b1e219f4.chunk.js"
+  },
+  {
+    "revision": "df4fb3cd79281c4a44ab",
+    "url": "https://.github.io/csb-l46wcf/static/js/main.9b0ca345.chunk.js"
+  },
+  {
+    "revision": "e6a79693b328889a4de4",
+    "url": "https://.github.io/csb-l46wcf/static/js/runtime-main.5cf05064.js"
+  }
+]);
